d4470b173457cc0c1b72eb697dd86bf20cc22c2e
chriskes <47326672+chriskes@users.noreply.github.com>
2020-11-06 00:36:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -36,13 +36,41 @@ window.addEventListener('scroll', function(){
 
 // BRON: https://codepen.io/leotran/pen/XdjMzj
 
-var current = 0,
-    slides = document.getElementsByClassName("slideimg");
+var current1 = 0;
+slides1 = document.getElementsByClassName("slideimg");
 
 setInterval(function () {
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.opacity = 0;
+    for (var i = 0; i < slides1.length; i++) {
+        slides1[i].style.opacity = 0;
     }
-    current = (current != slides.length - 1) ? current + 1 : 0;
-    slides[current].style.opacity = 1;
+    current1 = (current1 != slides1.length - 1) ? current1 + 1 : 0;
+    slides1[current1].style.opacity = 1;
 }, 4000);
+
+
+var current2 = 0;
+    slides2 = document.getElementsByClassName("slideimgsponsor");
+
+setInterval(function () {
+    for (var i = 0; i < slides2.length; i++) {
+        slides2[i].style.opacity = 0;
+    }
+    current2 = (current2 != slides2.length - 1) ? current2 + 1 : 0;
+    slides2[current2].style.opacity = 1;
+}, 4000);
+
+
+
+
+
+
+var searchbutton = document.querySelector(".searchbutton");
+var searchmenu = document.querySelector("#searchmenu");
+var body = document.querySelector("main");
+
+
+searchbutton.addEventListener("click", function() {
+    searchmenu.classList.toggle("visible")
+    header.classList.toggle("stickyheadersearch")
+    searchbutton.classList.toggle("underline")
+});
